e181ec7634f61fdcc9d13dc78cd21c6a32b7196f
Joker <1574427203@qq.com>
2022-09-11 11:43:48 +0800

    SpringMvc中的三种视图

---
+++ spring_mvc_demo/src/main/java/com/atguigu/controller/TestParamController.java
@@ -0,0 +1,66 @@
+package com.atguigu.controller;
+
+import com.atguigu.pojo.User;
+import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/*
+* 获取请求参数的方式
+* 1、通过servletAPI获取
+* 只需要在控制器方法的形参位置设置HttpServletRequest类型的形参
+* 就可以在控制器方法中使用request对象获取请求参数
+* 2. 通过控制器方法的形参获取
+* 只需要在控制器方法的形参位置，设置一个形参，形参的名字和请求的参数的名字一致
+* 3.@RequestParam: 将请求参数和控制器方法的形参绑定
+* @RequestParam注解的三个属性: value, required, defaultValue
+* value: 设置和形参绑定的请求参数的名字
+* required: 设置是否必须传输value所对应的请求参数
+* 默认值为true 标识value所对应的请求参数必须传输，否则页面报错
+* 若设置为false,则标识value所对应的请求参数不是必须传输，若为传输，则形参值为Null
+* defaultValue: 设置当没有传输value所对应的请求参数时，为形参设置默认值，此时和required的属性值一致
+* 4.@RequestHeader: 将请求头信息和控制器方法为的形参绑定
+* 5.CookieValue: 将cookie数据和控制器方法的形参绑定
+* 6. 通过控制器方法的实体类类型的形参获取请求参数
+* 需要在控制器方法的形参位置设置实体类类型的形参，要保证实体类中的属性的属性名和请求参数的名字一致
+* 7.解决获取请求此参数的乱码问题
+* 在web.xml中配置Spring的编码过滤器CharacterEncodingFilter
+* */
+@Controller
+public class TestParamController {
+
+    @RequestMapping("/param/servletAPI")
+    public String getParamByServletAPI(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("username:"+username+",password"+password);
+        return "success";
+    }
+
+
+    @RequestMapping("/param")
+    // 在一些特定的情况下会使用RequestParam指定请求的变量名
+    public String getParam(@RequestParam(value="userName", required = true, defaultValue = "hello") String username,
+                           String password
+                           //@RequestHeader(value = "referer") String referer,
+                           //@CookieValue("JSESSIONID") String jsessionId
+                           ){
+        //System.out.println("jsessionId:"+jsessionId);
+        //System.out.println("referer:"+referer);
+        System.out.println("username:"+username+",password"+password);
+        return "success";
+    }
+
+    @RequestMapping("/param/pojo")
+    public String getParamByPojo(User user){
+        System.out.println(user);
+        return "success";
+    }
+}
